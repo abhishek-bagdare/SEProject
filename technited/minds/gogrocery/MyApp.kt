111beb2e60e14b06a49c8b4f805bb32526f95181
Abhishek P Bagdare <90578089+abhishek-bagdare@users.noreply.github.com>
2023-04-18 18:00:23 -0400

    Add files via upload

---
+++ technited/minds/gogrocery/MyApp.kt
@@ -0,0 +1,16 @@
+//package technited.minds.gogrocery
+//
+//import android.app.Application
+//import androidx.databinding.library.baseAdapters.BuildConfig
+//import dagger.hilt.android.HiltAndroidApp
+//import timber.log.Timber
+//
+//@HiltAndroidApp
+//class MyApp : Application() {
+//    override fun onCreate() {
+//        super.onCreate()
+//        if (BuildConfig.DEBUG) {
+//            Timber.plant(Timber.DebugTree())
+//        }
+//    }
+//}
