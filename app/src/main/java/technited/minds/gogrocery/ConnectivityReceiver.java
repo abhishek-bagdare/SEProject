dbad0921797b17cf8138d4bfa6bc8d91949c033b
Abhishek P Bagdare <bagdare.a@northeastern.edu>
2023-04-18 18:53:53 -0400

    test

---
+++ app/src/main/java/technited/minds/gogrocery/ConnectivityReceiver.java
@@ -0,0 +1,41 @@
+package technited.minds.gogrocery;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class ConnectivityReceiver
+        extends BroadcastReceiver {
+
+    public static ConnectivityReceiverListener connectivityReceiverListener;
+
+    public ConnectivityReceiver() {
+        super();
+    }
+
+    public static boolean isConnected() {
+       return true;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null
+                && activeNetwork.isConnectedOrConnecting();
+
+        if (connectivityReceiverListener != null) {
+            connectivityReceiverListener.onNetworkConnectionChanged(isConnected);
+        }
+    }
+
+
+    public interface ConnectivityReceiverListener {
+        void onNetworkConnectionChanged(boolean isConnected);
+    }
+
+}
---
+++ app/src/main/java/technited/minds/gogrocery/HomeActivity.java
@@ -0,0 +1,49 @@
+//package technited.minds.gogrocery;
+//
+//import android.annotation.SuppressLint;
+//import android.app.AlertDialog;
+//import android.app.SearchManager;
+//import android.content.Context;
+//import android.content.DialogInterface;
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.os.AsyncTask;
+//import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.GridView;
+//import android.widget.LinearLayout;
+//import android.widget.ProgressBar;
+//import android.widget.Toast;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+//
+//
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.OutputStream;
+//import java.io.OutputStreamWriter;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//import java.net.URLEncoder;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.List;
+//
+//import technited.minds.gogrocery.AddorRemoveCallbacks;
+//
+//public class HomeActivity extends AppCompatActivity
+//        {
+//
+//}
